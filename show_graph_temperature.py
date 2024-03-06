0712b5fcb0c876e27ddca16b5434a8e3566a2776
Sarah <sglaesser@yahoo.de>
2024-03-06 12:24:33 +0100

    Added a temperature graph which is generated in a python script.

---
+++ show_graph_temperature.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3 
+
+import rrdtool
+
+filename = "/var/www/html/temperature1.rrd"
+
+ret = rrdtool.graph ('-',
+		'--imgformat', 'PNG',
+		'--width', '640',
+		'--height', '400',
+		'--start', 'now-1hour',
+		'--end', 'now',
+		'--vertical-label', "Grad Celsius",
+		'--alt-autoscale',
+		'--title', 'Temperatur',
+		'DEF:temp1='+filename+':temp1:AVERAGE',
+		'AREA:temp1#ADD8E6:',
+		'LINE1:temp1#006400:Temperatur DS18B20',
+		'GPRINT:temp1:MIN:Min\\: %3.2lf °C',
+		'GPRINT:temp1:MAX:Max\\: %3.2lf °C',
+		'GPRINT:temp1:AVERAGE:Avg\\: %3.2lf °C',
+		'GPRINT:temp1:LAST:Aktuell\\: %3.2lf °C') 
+
+
+
